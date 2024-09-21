b37cd9a62dc3d6920a666c53eefd39c3885f472b
Mesterkaa <32128511+Mesterkaa@users.noreply.github.com>
2024-09-21 17:28:27 +0200

    Removed overridden name

--- octoprint_psu_control_ikea_dirigera/__init__.py
+++ octoprint_psu_control_ikea_dirigera/__init__.py
@@ -119,7 +119,7 @@ class Psu_control_ikea_dirigeraPlugin(octoprint.plugin.SettingsPlugin,
 # If you want your plugin to be registered within OctoPrint under a different name than what you defined in setup.py
 # ("OctoPrint-PluginSkeleton"), you may define that here. Same goes for the other metadata derived from setup.py that
 # can be overwritten via __plugin_xyz__ control properties. See the documentation for that.
-__plugin_name__ = "PSU Control - IKEA Dirigera"
+## __plugin_name__ = "PSU Control - IKEA Dirigera"
 
 
 # Set the Python version your plugin is compatible with below. Recommended is Python 3 only for all new plugins.
